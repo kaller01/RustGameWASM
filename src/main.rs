2a7450834bf9987176e4fe03dd05b81c2ee62fa1
kaller01 <martin@kallers.se>
2022-12-31 23:03:48 +0100

    Touch support

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,61 @@
 use macroquad::prelude::*;
 use player::{Entity, Player};
+use macroquad_virtual_joystick::{Joystick, JoystickDirection};
 
 pub mod player;
 pub mod world;
 use crate::world::World;
 
-use macroquad_virtual_joystick::Joystick;
+
 
 #[macroquad::main("2D")]
+#[tokio::main]
 async fn main() {
+    let mut screen_size = (screen_width(), screen_height());
+    let mut size = if screen_width() > screen_height() {
+        screen_width()
+    } else {
+        screen_height()
+    };
+    //Changable settings (camera, player etc)
     let mut target = vec2(0., 0.);
     let mut z = 0.05;
     let mut camera_follow_player = true;
     const MAX_ZOOM: f32 = 0.01;
     let mut generate_terrain = true;
+    let mut touch_controll = false;
 
     let mut world = World::generate();
     let mut player = Player::new(0., 0.);
-    let mut joystick = Joystick::new(screen_width() * 0.8, screen_height() * 0.8, 200.0);
-
-    let mut screen_size = (screen_width(), screen_height());
+    let mut player_joystick =
+        Joystick::new(screen_width() * 0.8, screen_height() * 0.8, size * 0.1);
+    let mut camera_joytstick =
+        Joystick::new(screen_width() * 0.2, screen_height() * 0.8, size * 0.1);
 
     loop {
         clear_background(LIGHTGRAY);
 
         if screen_size != (screen_width(), screen_height()) {
             screen_size = (screen_width(), screen_height());
-            joystick = Joystick::new(screen_width() * 0.8, screen_height() * 0.8, 200.0);
-        }
 
-        // joystick.set_center(screen_width() * 0.8, screen_height() * 0.8);
+            size = if screen_width() > screen_height() {
+                screen_width()
+            } else {
+                screen_height()
+            };
+
+            player_joystick =
+                Joystick::new(screen_width() * 0.8, screen_height() * 0.8, size * 0.1);
+            camera_joytstick =
+                Joystick::new(screen_width() * 0.2, screen_height() * 0.8, size * 0.1);
+        }
+        //Touch controls
+        if is_key_pressed(KeyCode::T) {
+            touch_controll = !touch_controll;
+        }
+        if !touch_controll && !touches().is_empty() {
+            touch_controll = true;
+        }
 
         //Handle camera controlls
         {
@@ -58,6 +84,15 @@ async fn main() {
             if is_key_down(KeyCode::F) {
                 camera_follow_player = true;
             }
+
+            if touch_controll {
+                let joystick_event = camera_joytstick.update();
+                match joystick_event.direction {
+                    JoystickDirection::Up => z *= 1.01,
+                    JoystickDirection::Down => z *= 0.99,
+                    _ => (),
+                }
+            }
         }
 
         //handle player controlls
@@ -79,9 +114,8 @@ async fn main() {
             player.set_velocity(velocity);
 
             //Joystick
-            {
-                let joystick_event = joystick.update();
-                println!("{:?}, {:?}", joystick_event.direction.to_local(), joystick_event.intensity);
+            if touch_controll {
+                let joystick_event = player_joystick.update();
                 player.set_velocity(
                     joystick_event.direction.to_local()
                         * joystick_event.intensity
@@ -100,7 +134,8 @@ async fn main() {
         set_camera(&Camera2D {
             target: target,
             zoom: zoom,
-            // viewport: Some((0,0,(screen_width()/2.).round() as i32,screen_height().round() as i32)),
+            // viewport: Some((0,0,800,800)),
+            // viewport: Some((0,(0.2*screen_height()).round() as i32,(screen_width()).round() as i32,(0.8*screen_height()).round() as i32)),
             ..Default::default()
         });
 
@@ -133,7 +168,6 @@ async fn main() {
         }
 
         set_default_camera();
-        joystick.render();
 
         draw_text(
             "ARROWS to move player (purple circle)",
@@ -146,6 +180,11 @@ async fn main() {
         draw_text("WASD to move camera", 30.0, 90.0, 30.0, BLACK);
         draw_text("F to follow player", 30.0, 120.0, 30.0, BLACK);
         draw_text("G to toggle generation", 30.0, 150.0, 30.0, BLACK);
+        draw_text("T to toggle touch controls", 30.0, 180.0, 30.0, BLACK);
+        if touch_controll {
+            player_joystick.render();
+            camera_joytstick.render();
+        }
         if z <= MAX_ZOOM {
             draw_text("Max zoom reached, pink is camera border, see the chunks load in and out as you move camera", 30.0, screen_height()*0.95, 30.0, BLACK);
         }
